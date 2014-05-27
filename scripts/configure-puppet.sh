88c1b0213a8887c4dc57e57ba1dbcfd610142127
pandrew <letters@paulnotcom.se>
2014-05-28 00:50:22 +0200

    add configure script for puppet

---
+++ scripts/configure-puppet.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# configure puppet to look for the puppetmaster at a specific
+# machine. I really don't like the default of always naming
+# the puppet master "puppet". This gets around that.
+
+cat > /etc/default/puppet <<EOF
+# Defaults for puppet - sourced by /etc/init.d/puppet
+
+# Start puppet on boot?
+START=yes
+
+# Startup options
+DAEMON_OPTS=""
+EOF
+
+cat > /etc/puppet/puppet.conf <<EOF
+[main]
+logdir=/var/log/puppet
+vardir=/var/lib/puppet
+ssldir=/var/lib/puppet/ssl
+rundir=/var/run/puppet
+factpath=$vardir/lib/facter
+templatedir=$confdir/templates
+server=my-puppet-master.mydomain.com
+EOF
