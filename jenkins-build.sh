b9df6921e4f1faff169c177c3fe561e287f97205
pandrew <letters@paulnotcom.se>
2014-06-02 22:13:37 +0200

    update jenkins build script

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -1,8 +1,21 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 cd $WORKSPACE
-$(which packer) build ./ubuntu-current-virtualbox.json
 
-if [ -a $WORKSPACE/packer-ubuntu-current.box ]; then
-rm $WORKSPACE/packer-ubuntu-current.box
-fi
+# Pre cleanup
+vboxmanage controlvm packer-ubuntu-current poweroff || true
+vboxmanage unregistervm packer-ubuntu-current --delete || true
+rm -rf $WORKSPACE/packer/output-ubuntu-current || true
+rm -rf /var/lib/jenkins/VirtualBox\ VMs/packer-ubuntu-current || true
+
+$(which packer) build ./ubuntu-current-virtualbox.json || exit 1;
+
+
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+
+$(which gof3r) put -c 1 -p $WORKSPACE/packer-ubuntu-current.box -b 1213212412134 -k vagrant/packer-ubuntu-current.box || exit 1;
+
+# Post cleanup
+rm $WORKSPACE/packer-ubuntu-current.box || true
+
